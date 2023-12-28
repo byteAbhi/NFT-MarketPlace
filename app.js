5a913ef0cbf1e109d737c0c23d27f98d2d58f5bf
Abhishek Mukhiya <abhishekmukhiya56321@gmail.com>
2023-12-29 02:43:08 +0530

    file added successfully

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  connectTimeout: 60000, // 60 seconds
   };
 
   const sessionStore = new MySQLStore({
