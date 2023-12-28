027aca88950967d3f5955a4044a3aae600b8644a
Abhishek Mukhiya <abhishekmukhiya56321@gmail.com>
2023-12-29 02:17:50 +0530

    file added successfully

--- app.js
+++ app.js
@@ -25,10 +25,10 @@ const secretKey = process.env.SESSION_SECRET;
 
 //creating connection
 const dbConfig = {
-  host: dbHost,
-  user: dbUser,
-  password: dbPassword,
-  database: dbName
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
   };
 
   const sessionStore = new MySQLStore({
